e4d03dff74931bf475c0c8a3265915b684f00fd5
jaewonKil <jw.kil@ukmedia.co.kr>
2024-09-02 15:11:01 +0900

    타입 선언 중복 제거

--- src/test/kotlin/SetTest.kt
+++ src/test/kotlin/SetTest.kt
@@ -3,8 +3,8 @@ import io.kotest.matchers.shouldBe
 
 class SetTest : FunSpec({
     test("Set mutable, immutable") {
-        val aNewIssue: String = "uniqueDescr4"
-        val anIssueAlreadyIn: String = "uniqueDescr2"
+        val aNewIssue = "uniqueDescr4"
+        val anIssueAlreadyIn = "uniqueDescr2"
 
         "Issue $aNewIssue ${getStatusLog(addIssue(aNewIssue))}" shouldBe "Issue uniqueDescr4 registered correctly."
         "Issue $anIssueAlreadyIn ${getStatusLog(addIssue(anIssueAlreadyIn))}" shouldBe "Issue uniqueDescr2 marked as duplicate and rejected."
