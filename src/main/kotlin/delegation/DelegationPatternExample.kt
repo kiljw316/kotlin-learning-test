e66b320f4304edb422f3c32de9b9d308f7b60f63
jaewonKil <jw.kil@ukmedia.co.kr>
2024-09-03 11:11:43 +0900

    Delegation Pattern example 추가

---
+++ src/main/kotlin/delegation/DelegationPatternExample.kt
@@ -0,0 +1,24 @@
+package delegation
+
+interface SoundBehavior {
+    fun makeSound()
+}
+
+class ScreamBehavior(val n: String) : SoundBehavior {
+    override fun makeSound() = println("${n.uppercase()} !!!")
+}
+
+class RockAndRollBehavior(val n: String) : SoundBehavior {
+    override fun makeSound() = println("I'm The King of Rock 'N' Roll: $n")
+}
+
+class TomAraya(n: String) : SoundBehavior by ScreamBehavior(n)
+
+class ElvisPresley(n: String) : SoundBehavior by RockAndRollBehavior(n)
+
+fun main() {
+    val tomAraya = TomAraya("Thrash Metal")
+    tomAraya.makeSound()
+    val elvisPresley = ElvisPresley("dancin' to the Jailhouse Rock.")
+    elvisPresley.makeSound()
+}
